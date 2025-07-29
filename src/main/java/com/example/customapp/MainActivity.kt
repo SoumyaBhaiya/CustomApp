fe39bbcb4a986ad8db03cbe265de11d81b198f74
Soumya Bhaiya <soumybhaiya@gmail.com>
2025-07-29 11:36:28 -0700

    edited a bit

--- src/main/java/com/example/customapp/MainActivity.kt
+++ src/main/java/com/example/customapp/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.customapp
 
+import android.adservices.topics.Topic
 import android.drm.DrmStore.RightsStatus
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,6 +15,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -20,8 +23,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.platform.InspectableModifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,30 +46,44 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BhaiyaImage(modifier = Modifier)
-                    BhaiyasHeading("Bhaiyas", "in development", modifier = Modifier)
-                    GeneralPurposeText(
-                        "This is some general purpose text.", modifier = Modifier
-                    )
+                    val logo = painterResource(R.drawable.bhaiyas)
+                    ContactCard(logo, "Bhaiyas", "1235", "@iasdfi")
                 }
             }
         }
     }
 }
+
+
 @Composable
-fun BhaiyasHeading(message: String, from: String, modifier: Modifier = Modifier){
-    Column(verticalArrangement = Arrangement.Top,
+fun ContactCard(logo: Painter, companyName: String, phoneNumber: String, socialMedia: String){
+    Column(
+        modifier = Modifier.padding(5.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceAround
+    ){
+        Image(painter = logo, contentDescription = "Bhaiya's Logo", modifier = Modifier.size(100.dp))
+        Text(text = companyName, modifier = Modifier)
+        Text(text = "\uD83D\uDCDE $phoneNumber")
+        Text(text = socialMedia)
+    }
+}
+
+
+@Composable
+fun BhaiyasHeading(message: String, smallText: String, modifier: Modifier = Modifier){
+    Column(verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier) {
         Text(
             text = message,
             fontSize = 70.sp,
-            lineHeight = 26.sp,
+            lineHeight = 16.sp,
             textAlign = TextAlign.Right
         )
         Text(
-            text = from,
+            text = smallText,
             fontSize = 16.sp,
-            modifier = Modifier.padding(8.dp).align(alignment = Alignment.End)
+            modifier = Modifier.padding(1.dp)
         )
     }
 
@@ -83,11 +105,13 @@ fun GeneralPurposeText(sometext:String, modifier: Modifier ){
 @Composable
 fun BhaiyaImage(modifier: Modifier){
     val image = painterResource(R.drawable.bhaiyas)
-    Box(modifier) {
+    Box(modifier= modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center) {
         Image(
             painter = image,
             contentDescription = null,
-            modifier = modifier
+            alpha = 0.5F,
+            modifier = modifier.size(100.dp)
 
         )
     }
